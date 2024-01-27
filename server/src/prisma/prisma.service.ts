3a101274959e25d411cca4ecbf384967ad875e10
Thailo Eduardo <thailoeduardo@gmail.com>
2024-01-26 23:21:11 -0300

    Set Up Swagger

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,4 +1,7 @@
+// src/prisma/prisma.service.ts
+
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {}
